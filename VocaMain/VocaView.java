6d037a7342c0da7b41a6996f5dfdb0c7e926f563
Hantaeyong <hhhhgong3@naver.com>
2023-10-05 17:16:57 +0900

    # Voca 실행을 하면 파일과 단어장 폴더가 추가되게 구현

--- VocaMain/VocaView.java
+++ VocaMain/VocaView.java
@@ -1,19 +1,25 @@
 package VocaMain;
 
+import java.util.Map;
+
 import static VocaMain.Utility.input;
 import static VocaMain.Utility.makeLine;
 
 public class VocaView {
 
+    private static VocaFile vocaFile; // VocaFile 객체를 클래스 레벨로 이동
+
     public static void start(){
 
+        vocaFile = new VocaFile(); //1번 선택 전에 VocaFile 객체 생성 ( 파일 생성)
+
         while (true){
-            System.out.println("\n\n****** 👨‍💻단어Voca 프로그램👩‍💻 ******");
+            System.out.println("\n\n****** 👨‍💻Voca 프로그램👩‍💻 ******");
             System.out.println("#1. 단어장 보기!👀");
             System.out.println("#2. 객관식 문제 풀기!😊");
             System.out.println("#3. 주관식 문제 풀기!🙂");
             System.out.println("#4. 새로운 단어 추가!🧠");
-            System.out.println("#5. 단어Voca 프로그램 종료🙌");
+            System.out.println("#5. Voca 프로그램 종료🙌");
 
             makeLine();
 
@@ -22,19 +28,18 @@ public class VocaView {
             switch (menuNum) {
                 case "1":
                     break;
-                case"2":
+                case "2":
                     break;
-                case"3":
+                case "3":
                     break;
-                case"4":
+                case "4":
                     break;
-                case"5":
+                case "5":
+                    System.out.println("수고하셨습니다!👍");
                     System.exit(0);
                 default:
                     System.out.println("# 입력 하신 메뉴번호가 잘못 됐습니다!😅");
             }
-
         }
-
     }
 }
