48df416c51c4d2cec6e82ef0ca1e7f70d795d0b1
MinGeon Song <smg000218@gmail.com>
2023-10-05 17:18:20 +0900

    PlusWord 기능완성

--- MinGeon/PlusWord.java
+++ MinGeon/PlusWord.java
@@ -1,30 +1,68 @@
 package MinGeon;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 
 import static MinGeon.Utility.input;
 
 public class PlusWord {
-    static final String ROOT_PATH = "C:";
+    static final String ROOT_PATH = "D:";
 
     public PlusWord() {
-        File file = new File(ROOT_PATH + "plusWord.txt");
+        File file = new File(ROOT_PATH + "/plusWord.txt");
+        FileReader fileReader = null;
+        String wordList = "";
+        String word = "";
+        String wordMean = "";
 
-        try (FileInputStream fis = new FileInputStream(file)) {
-            int data = 0;
-            while (data != -1) {
-                data = fis.read();
-                System.out.write(data);
+        if(!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("파일 생성 실패!");
+                return;
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("파일 불러오기 실패!");
         }
 
-        String word = input("단어 입력 : ");
-        String wordMean = input("단어의 뜻 입력 : ");
-        word = word + " ";
+        try {
+            fileReader = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("파일 불러오기 실패!");
+            return;
+        }
+
+        try (BufferedReader buffRead = new BufferedReader(fileReader)) {
+            String line;
+            while((line = buffRead.readLine()) != null) {
+                wordList = wordList + line +"\n";
+            }
+            fileReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("파일 읽어오기 실패!");
+            return;
+        }
+
+//        System.out.println(wordList); //잘 읽어오는지 확인
+
+        while(true) {
+            System.out.println("단어를 추가합니다. 0을 입력시 종료");
+            word = input("단어 입력 : ");
+            if (word.equals("0"))
+                break;
+            wordMean = input("단어의 뜻 입력 : ");
+            if (wordMean.equals("0"))
+                break;
+            word = word + " ";
+            wordList = wordList + word + wordMean + "\n";
+        }
+
+        try (FileWriter fileWriter = new FileWriter(file)){
+            fileWriter.write(wordList);
+            fileWriter.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
